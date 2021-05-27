c2011db1c251ade3aa0ba64a74be35866ea956a4
Tuca Madalin <50795013+tucamadalin99@users.noreply.github.com>
2021-05-27 22:07:27 +0300

    ML-26-27 - Update

--- src/app/liked/liked.component.ts
+++ src/app/liked/liked.component.ts
@@ -1,30 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../firebase.service';
+import { Recipe } from '../models/Recipe';
 
 @Component({
   selector: 'app-liked',
   templateUrl: './liked.component.html',
-  styleUrls: ['./liked.component.scss']
+  styleUrls: ['./liked.component.scss', '../app.component.scss']
 })
 export class LikedComponent implements OnInit {
-  recipe: any;
-  isLiked = false;
-  constructor(private fbService: FirebaseService) { }
+  recipe!: Recipe;
+  constructor(private fbService: FirebaseService) {
+
+  }
 
   ngOnInit(): void {
-    this.fbService.getRecipe(0).snapshotChanges().subscribe((recipe: any) => {
-      this.recipe = recipe;
-      if (this.recipe.liked) {
-        this.isLiked = true;
-      }
-
+    this.fbService.getRecipe(0).valueChanges().subscribe((recipe: any) => {
+      this.recipe = new Recipe(recipe);
     })
   }
 
   addLike(): void {
     this.fbService.updateProps(0, { liked: !this.recipe.liked }).then(() => {
-      this.recipe.liked = this.recipe.liked ? !this.recipe.liked : this.recipe.liked;
-      this.isLiked = !this.isLiked;
+      this.recipe.liked = this.recipe.liked ? true : false;
     });
 
   }
