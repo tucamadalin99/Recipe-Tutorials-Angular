5f7f6251b8937248a5099e6de796572df76bf6f0
Tuca Madalin <50795013+tucamadalin99@users.noreply.github.com>
2021-06-02 14:31:16 +0300

    ML-25 - Fixed repetead items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,14 @@ export class AppComponent {
     this.firebaseService.getCartInfo().valueChanges().subscribe(cart => {
       if (cart) {
         this.cart = new Object(cart);
+        let counts = {};
+        let formattedArr = [];
+        //Counting repetitions of items
+        this.cart.cartItems.forEach((x) => { counts[x.toLowerCase()] = (counts[x.toLowerCase()] || 0) + 1; });
+        for (let prop in counts) {
+          formattedArr.push(`${prop} ${counts[prop]}x`);
+        }
+        this.cart.cartItems = formattedArr;
       }
     })
   }
