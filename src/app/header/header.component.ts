b249c099af68d158956febb9e6ce024b0b47da41
Tuca Madalin <50795013+tucamadalin99@users.noreply.github.com>
2021-06-16 19:26:35 +0300

    ML-13 - Review

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../firebase.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-header',
@@ -10,7 +12,7 @@ export class HeaderComponent implements OnInit {
 
   cart: any = new Object(null);
 
-  constructor(private firebaseService: FirebaseService) { }
+  constructor(private firebaseService: FirebaseService, private route: Router) { }
 
   private getInfo(): void {
     this.firebaseService.getCartInfo().valueChanges().subscribe(cart => {
@@ -41,7 +43,11 @@ export class HeaderComponent implements OnInit {
   }
 
   public searchItem(value) {
-    console.log(`You searched for ${value}`);
+    if (!value) {
+      this.route.navigate(['/search', "all-categories", "all-items"]);
+    } else {
+      this.route.navigate(['/search', "all-categories", value]);
+    }
   }
 
   ngOnInit() {
